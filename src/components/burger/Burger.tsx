1ad77b3e97e4931098ecda6b1526937226fe2ff7
ArtsiomChults <artemorsha@mail.ru>
2024-01-10 01:48:31 +0300

    fix: style add className BurgerComponent to props

--- src/components/burger/Burger.tsx
+++ src/components/burger/Burger.tsx
@@ -1,10 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, FC} from 'react';
 import s from "./burger.module.scss";
 
-export const Burger = () => {
+type BurgerType = {
+    className?: string;
+}
+
+export const Burger: FC<BurgerType> = ({className}) => {
     const [isBurgerCross, setIsBurgerCross] = useState(false);
 
-    const classesForBurger = `${s.burger} ${isBurgerCross ? s.burgerCross : ''}`;
+    const classesForBurger = `${className} ${s.burger} ${isBurgerCross ? s.burgerCross : ''}`;
 
     const onClickBurger = () => {
         setIsBurgerCross(!isBurgerCross)
