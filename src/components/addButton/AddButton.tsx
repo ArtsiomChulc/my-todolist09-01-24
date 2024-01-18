e513a53eb28da6a688fd57fd43f64aa820c0cb43
ArtsiomChults <artemorsha@mail.ru>
2024-01-18 22:01:39 +0300

    add: filtered tasks

--- src/components/addButton/AddButton.tsx
+++ src/components/addButton/AddButton.tsx
@@ -1,15 +1,22 @@
-import React, {FC} from 'react';
+import React, {FC, MouseEventHandler} from 'react';
 
 type AddButtonType = {
-    nameBtn: string
-    className: string
-    onClick: () => void
+    titleBtn: string
+    className?: string
+    onClickCB?: (event: string) => void
+    value?: string
 }
 
-export const AddButton: FC<AddButtonType> = ({className, nameBtn, onClick}) => {
+export const AddButton: FC<AddButtonType> = ({className, titleBtn, onClickCB, value}) => {
+    const onClickHandler: MouseEventHandler<HTMLButtonElement> = (event) => {
+        const buttonValue = event.currentTarget.value
+        if (onClickCB) {
+            onClickCB(buttonValue)
+        }
+    }
     return (
         <>
-            <button onClick={onClick} className={className}><p>{nameBtn}</p></button>
+            <button onClick={onClickHandler} value={value} className={className}><p>{titleBtn}</p></button>
         </>
     );
 };
--- src/todos/todoList/TodoList.tsx
+++ src/todos/todoList/TodoList.tsx
@@ -1,6 +1,7 @@
-import React, {FC, ChangeEvent} from 'react';
+import React, {FC, ChangeEvent, useState} from 'react';
 import {AddButton} from "../../components/addButton/AddButton";
 import {changeTaskStatus, removeTask} from "../../reducers/tasksReducer";
+import {FilteredStateType} from "../../store/mockData";
 import {useAppDispatch} from "../../store/store";
 import {TaskType} from "../../types/taskType";
 
@@ -13,6 +14,19 @@ type TodoListType = {
 
 export const TodoList: FC<TodoListType> = ({titleTodo, tasks}) => {
 
+    const [filter, setFilter] = useState<FilteredStateType>('All')
+
+    const filteredTasks = tasks.filter(task => {
+        if (filter === 'All') {
+            return true
+        } else if (filter === 'Active') {
+            return !task.isDone
+        } else if (filter === 'Completed') {
+            return task.isDone
+        }
+        return true
+    })
+
     const dispatch = useAppDispatch()
 
     const onChangeStatus = (id: string, isDone: boolean) => {
@@ -23,11 +37,19 @@ export const TodoList: FC<TodoListType> = ({titleTodo, tasks}) => {
         dispatch(removeTask(id))
     }
 
+    const getNameBtn = (e: string) => {
+        setFilter(e as FilteredStateType)
+    }
+
+    const classBtnAll = `${s.groupButton} ${filter === 'All' ? s.active : ''}`
+    const classBtnActive = `${s.groupButton} ${filter === 'Active' ? s.active : ''}`
+    const classBtnCompleted = `${s.groupButton} ${filter === 'Completed' ? s.active : ''}`
+
     return (
         <section className={s.todoListContainer}>
             <p className={s.nameTodo}>{titleTodo}</p>
             <ul className={s.tasksWrap}>
-                {tasks.map(({taskTitle, id, isDone}) => {
+                {filteredTasks.map(({taskTitle, id, isDone}) => {
                     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
                         onChangeStatus(id, e.target.checked)
                     }
@@ -38,18 +60,15 @@ export const TodoList: FC<TodoListType> = ({titleTodo, tasks}) => {
                         <li key={id} className={s.taskInfo}>
                             <input type="checkbox" className={s.taskInput} onChange={onChangeHandler} checked={isDone}/>
                             <p className={s.taskTitle}>{taskTitle}</p>
-                            <AddButton nameBtn={'x'} className={s.taskButton} onClick={onClickHandler}/>
+                            <AddButton titleBtn={'x'} className={s.taskButton} onClickCB={onClickHandler}/>
                         </li>
                     )
                 })}
             </ul>
             <div className={s.wrapGroupBtn}>
-                <AddButton nameBtn={'All'} className={s.groupButton} onClick={() => {
-                }}/>
-                <AddButton nameBtn={'Active'} className={s.groupButton} onClick={() => {
-                }}/>
-                <AddButton nameBtn={'Completed'} className={s.groupButton} onClick={() => {
-                }}/>
+                <AddButton value={'All'} titleBtn={'All'} className={classBtnAll} onClickCB={getNameBtn}/>
+                <AddButton value={'Active'} titleBtn={'Active'} className={classBtnActive} onClickCB={getNameBtn}/>
+                <AddButton value={'Completed'} titleBtn={'Completed'} className={classBtnCompleted} onClickCB={getNameBtn}/>
             </div>
         </section>
     );
